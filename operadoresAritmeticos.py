1fc3450d9c2c39b5de39039ffb45e0112134c8b4
PABLO HENRI DAVID CIPRIANO <202503944482@alunos.unifanor.edu.br>
2025-03-28 10:27:59 -0300

    operadoresAritmeticos

---
+++ operadoresAritmeticos.py
@@ -0,0 +1,36 @@
+#Adição
+numero1=10
+numero2=20
+
+soma= numero1 + numero2
+print(soma)
+
+#Subtração
+
+subtraçao = numero1 - numero2
+print(subtraçao)
+
+# Multiplicação
+
+multiplicaçao=numero1 * numero2
+print(multiplicaçao)
+
+#Divisão
+
+divisao=numero2/numero1
+print(divisao)
+
+#Potência 
+
+potencia=numero1**numero1
+print(potencia)
+
+#Modulo
+
+modulo=numero1%numero2
+print(modulo)
+
+#Resto da divisão
+
+restodadivisao= numero1//numero2
+print(restodadivisao)
